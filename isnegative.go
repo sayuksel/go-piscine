d7ad445c18ac585cc6ebfa2655b57cbd6690f414
sayuksel <saloohyplayz2005@gmail.com>
2024-02-19 14:19:59 +0300

    hh

---
+++ isnegative.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func IsNegative(num int) {
+	if num < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
+}
