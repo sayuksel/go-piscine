7bd775f51d3b4f3908050a41d56e68342ca19778
sayuksel <saloohyplayz2005@gmail.com>
2024-02-21 13:22:42 +0300

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,22 @@
+package piscine
+
+/*func BasicAtoi(s string) {
+	arr_s := []rune(s)
+	arr_int := []int{}
+
+	for i := 0; i < len(arr_s)-1; i++ {
+		arr_int = append(arr_int, int(arr_s[i]))
+	}
+	int_s := arr_int
+	return int_s
+}
+*/
+
+func BasicAtoi(s string) int {
+	result := 0 
+
+	for i := 0; i < len(s); i++ {
+		result = (result * 10) + int(s[i] - 48)
+	}
+	return result
+}
