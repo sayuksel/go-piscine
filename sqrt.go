85e0306fc69d56cd906d8a4da8133937beb9ea45
sayuksel <saloohyplayz2005@gmail.com>
2024-02-25 14:12:04 +0300

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,9 +1,24 @@
 package piscine
 
-func Sqrt(nb int) int {
-	sqrt := nb * nb
-	if sqrt%2 == 0 {
-		return sqrt
+// func Sqrt(nb int) int {
+// 	sqrt := nb * nb
+// 	if sqrt%2 == 0 {
+// 		return sqrt
+// 	} else {
+// 		return 0
+// 	}
+// }
+
+func Sqrt(x int) int {
+	// Set the initial guess for the square root
+	guess := x / 2
+
+	// Iterate until desired precision is achieved
+	for i := 0; i < 100; i++ {
+		guess = ((guess) + x/guess) / 2
+	}
+	if guess%2 == 0 {
+		return guess
 	} else {
 		return 0
 	}
