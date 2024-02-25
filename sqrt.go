0feb71455c51bd1bab0b2030c0ee800789a236ee
sayuksel <saloohyplayz2005@gmail.com>
2024-02-25 14:57:56 +0300

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -3,7 +3,15 @@ package piscine
 func Sqrt(nb int) int {
 	prod := 0
 
-	for i := 2; i < nb; i++ {
+	if nb == 1 {
+		return 1
+	}
+
+	if nb == 0 {
+		return 0
+	}
+
+	for i := 0; i < nb; i++ {
 		prod = i * i
 		if prod == nb {
 			return i
