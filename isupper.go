19ecec9f5c56ac9480cbcc956e0efe9e65afcf9f
sayuksel <saloohyplayz2005@gmail.com>
2024-02-26 12:35:32 +0300

    isupper

---
+++ isupper.go
@@ -0,0 +1,17 @@
+package piscine
+
+func IsUpper(s string) bool {
+	flag := false
+	for _, ch := range s {
+		flag = false
+		if ch >= 'A' && ch <= 'Z' {
+			flag = true
+		}
+	}
+	if flag {
+		return true
+	} else {
+		return false
+	}
+
+}
