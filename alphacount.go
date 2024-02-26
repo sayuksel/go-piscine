c04f9c6df502aa18557d7d963c8fff669f5c975f
sayuksel <saloohyplayz2005@gmail.com>
2024-02-26 11:23:40 +0300

    alpha count

---
+++ alphacount.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AlphaCount(s string) int {
+
+	alpha := 0
+
+	for _, ch := range s {
+		if ch >= 'a' && ch <= 'z' {
+			alpha++ 
+		} else if ch >= 'A' && ch <= 'Z' {
+			alpha++
+		}
+	}
+	return alpha
+}
