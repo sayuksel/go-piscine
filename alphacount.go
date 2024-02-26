11f75a150fdecffa4ac80e13e0a8526b9112cc24
sayuksel <saloohyplayz2005@gmail.com>
2024-02-26 11:23:50 +0300

    alpha count

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,11 @@
 package piscine
 
 func AlphaCount(s string) int {
-
 	alpha := 0
 
 	for _, ch := range s {
 		if ch >= 'a' && ch <= 'z' {
-			alpha++ 
+			alpha++
 		} else if ch >= 'A' && ch <= 'Z' {
 			alpha++
 		}
