3ee8d0e9e8ead5555765ef379f21856fff52abc1
sayuksel <saloohyplayz@gmail.com>
2024-02-27 20:48:43 +0300

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,25 +3,51 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	arr := []int{}
-	remainder := 0
 
-	for i := 0; i <= len(string(n+1)); i++ {
-		remainder = n % 10
-		n /= 10
-		arr[i] = remainder
-
+	if n == 0 {
+		z01.PrintRune('0')
 	}
 
-	for i := 0; i <= len(arr)-1; i++ {
-		if arr[i] > arr[i+1] {
-			temp := arr[i]
-			arr[i] = arr[i+1]
-			arr[i+1] = temp
+	count := 0
+	temp := n
+	for temp > 0 {
+		temp /= 10
+		count++
+	}
+
+	// // Count the number of digits
+
+	arr := make([]int, count)
+	for i := count - 1; i >= 0; i-- {
+		arr[i] = n % 10
+		n /= 10
+	}
+
+	// // Extract the digits into an array
+
+	for i := 0; i < count-1; i++ {
+		for j := 0; j < count-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
 		}
 	}
 
-	for i := 0; i <= len(arr); i++ {
-		z01.PrintRune(rune(arr[i]))
+	// // Sort the digits in ascending order using bubble sort
+	// for i := 0; i < count-1; i++ {
+	// 	for j := 0; j < count-i-1; j++ {
+	// 		if digits[j] > digits[j+1] {
+	// 			digits[j], digits[j+1] = digits[j+1], digits[j]
+	// 		}
+	// 	}
+	// }
+
+	for _, ch := range arr {
+		z01.PrintRune(rune(ch) + 48)
 	}
 }
+
+// // Print the sorted digits
+// for _, digit := range digits {
+// 	z01.PrintRune(rune(digit)+'0')
+// }
---
+++ test/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	piscine.PrintNbrInOrder(312)
+	fmt.Println(" ")
+	piscine.PrintNbrInOrder(0)
+	fmt.Println(" ")
+	piscine.PrintNbrInOrder(321)
+}
