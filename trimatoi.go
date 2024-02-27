fcdbf82e15f747e939dc5e72aaf3fea75ae668f6
sayuksel <saloohyplayz@gmail.com>
2024-02-27 21:03:32 +0300

    trimatoi

---
+++ trimatoi.go
@@ -0,0 +1,12 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	str_arr := []rune(s)
+	int_arr := []int{}
+
+	for _, v := range arr {
+		if v >= '0' && v <= '9' {
+			int_arr = int(v) + 48
+		}
+	}
+}
