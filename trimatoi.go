aff39efca7c922fcc1193d48460a4ec629c58081
sayuksel <saloohyplayz@gmail.com>
2024-02-28 11:32:34 +0300

    trimatoi.go

--- trimatoi.go
+++ trimatoi.go
@@ -1,38 +1,20 @@
 package piscine
 
 func TrimAtoi(s string) int {
-
 	num := 0
 	IsNegative := false
 	IsNumber := false
 
-	for _, char := range s {
-		if char >= '0' && char <= '9' {
-			num = num * 10 + int(char) - '0'
-			IsNumber = true 
-		} else if char == '-' && IsNumber == false{
+	for _, v := range s {
+		if v >= '0' && v <= '9' {
+			num = num * 10 + int(v) - '0'
+			IsNumber = true
+		} else if v == '-' && !IsNumber {
 			IsNegative = true
-		} 
+		}
 	}
-
-	if IsNegative{ 
+	if IsNegative {
 		num *= -1
 	}
-
 	return num
-	// str_arr := []rune(s)
-	// //int_arr := []int{}
-	// var ret_val int
-	// flag := false
-
-	// for _, v := range str_arr {
-	// 	if v >= '0' && v <= '9' {
-	// 		ret_val = append(ret_val, int(v) + 48)
-	// 		flag = true
-	// 	} else if v == 45 && flag == false {
-	// 		ret_val = append(ret_val, '-')
-	// 	}
-	// }
-	// //ret_val = int_arr
-	// return ret_val
 }
