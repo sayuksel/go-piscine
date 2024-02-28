ea8d16f34dca9ac208904e71476ffb571e381e78
sayuksel <saloohyplayz@gmail.com>
2024-02-28 18:41:52 +0300

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	args := os.Args
-	
+
 	fullName := args[0]
 	trimmedName := fullName[2:]
 	for _, v := range trimmedName {
