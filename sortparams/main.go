40cf3d84643ee62b58a7ef9ad9b98324d19a2ad4
sayuksel <saloohyplayz@gmail.com>
2024-02-28 23:32:37 +0300

    sortparams

---
+++ sortparams/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func Print(s string) {
+	for _, v := range s {
+		z01.PrintRune(v)
+	}
+}
+
+func main() {
+
+	input := os.Args[1:]
+
+	for i := 0; i <= len(input)-1; i++ {
+		for j := 0; j <= len(input)-i-1; j++ {
+			if input[i] > input[j] {
+				input[i], input[j] = input[j], input[i]
+			}
+		}
+	}
+	for _, v := range input {
+		Print(v)
+		z01.PrintRune('\n')
+	}
+}
