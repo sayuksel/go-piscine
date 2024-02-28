7afd59a855de8abc16d010857d371da9f0526783
sayuksel <saloohyplayz@gmail.com>
2024-02-28 23:10:06 +0300

    revparams

--- revparams/main.go
+++ revparams/main.go
@@ -7,14 +7,12 @@ import (
 )
 
 func Print(s string) {
-
 	for _, v := range s {
 		z01.PrintRune(v)
 	}
 }
 
 func main() {
-
 	input := os.Args[1:]
 	for i := len(input) - 1; i >= 0; i-- {
 		Print(input[i])
