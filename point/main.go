e1eb0c9a9fbd0f960173eb81f5521bf58d223a5d
sayuksel <saloohyplayz2005@gmail.com>
2024-03-03 15:24:00 +0300

    point

--- point/main.go
+++ point/main.go
@@ -14,21 +14,41 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
-
+	a := ' '
 	setPoint(points)
 
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	z01.PrintRune('4')
-	z01.PrintRune('2')
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	z01.PrintRune('2')
-	z01.PrintRune('1')
+	for i := 0; i < 14; i++ {
+		switch i {
+		case 0:
+			a = 'x'
+		case 1:
+			a = ' '
+		case 2:
+			a = '='
+		case 3:
+			a = ' '
+		case 4:
+			a = '4'
+		case 5:
+			a = '2'
+		case 6:
+			a = ','
+		case 7:
+			a = ' '
+		case 8:
+			a = 'y'
+		case 9:
+			a = ' '
+		case 10:
+			a = '='
+		case 11:
+			a = ' '
+		case 12:
+			a = '2'
+		case 13:
+			a = '1'
+
+		}
+		z01.PrintRune(a)
+	}
 }
