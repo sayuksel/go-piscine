738d828613ea6360e622bb864cc81e499667ffcd
sayuksel <saloohyplayz2005@gmail.com>
2024-03-07 14:14:34 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -1,13 +1,14 @@
 package piscine
 
 func f(a, b int) int {
-	if a < b {
+	if a > b {
 		return 1
 	} else if a == b {
 		return 0
-	}
+	} 
 	return -1
 }
+
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	l := len(a)
