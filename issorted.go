93b140a244c28a9f402bb44def368507e1522117
sayuksel <saloohyplayz2005@gmail.com>
2024-03-07 13:52:19 +0300

    issorted

---
+++ issorted.go
@@ -0,0 +1,21 @@
+package piscine
+
+func f(a, b int) int {
+	if a > b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	retval := f(a[0], a[1])
+
+	if retval > 0 {
+		return true
+	} else {
+		return false
+	}
+}
